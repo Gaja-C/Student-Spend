a1acc3af69208cae7408dfbdd1b4f7eaa4b71366
isaachowse <3137760h@student.gla.ac.uk>
2026-03-12 16:21:46 +0000

    Little tweak to register

--- rango/models.py
+++ rango/models.py
@@ -5,6 +5,7 @@ from django.contrib.auth.models import User
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+    website = models.URLField(blank=True)
     picture = models.ImageField(upload_to='profile_images', blank=True)
 
     def __str__(self):
